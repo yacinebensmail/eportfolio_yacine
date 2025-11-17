d38fa561dec853f52bf59bc7458a3f5e736011e3
Yacine <yacine@yac.local>
2025-11-17 13:41:11 +0100

    Mise à jour du dossier / ajout des fichiers

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,5 +1,6 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@symfony/stimulus-bridge";
 
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+const app = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+app.load(definitionsFromContext(context));
