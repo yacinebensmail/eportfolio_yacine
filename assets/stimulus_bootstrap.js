a5e31f73ae31d7904ee58645f322429b84b0e733
Yacine <yacine@wifiroam050050.univ-st-etienne.fr>
2025-11-17 14:46:11 +0100

    Ajout animations CSS, slider et descriptions dans About et Skills

--- assets/stimulus_bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "@symfony/stimulus-bridge";
-
-const app = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
-app.load(definitionsFromContext(context));
--- public/index.php
+++ public/index.php
@@ -1,9 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio Yacine</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 
-use App\Kernel;
+<h1>Bienvenue sur mon portfolio</h1>
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+<!-- Slider -->
+<div class="slider">
+    <img src="images/slide1.jpg" class="slide">
+    <img src="images/slide2.jpg" class="slide">
+    <img src="images/slide3.jpg" class="slide">
+</div>
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+<!-- Boutons animés -->
+<button>Contact</button>
+<button>Projets</button>
+
+</body>
+</html>
